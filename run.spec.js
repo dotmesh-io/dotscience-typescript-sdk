140aca3f318bec009e50978028d54b52f14707ba
Charlotte Godley <charlotte@dotmesh.com>
2019-08-30 15:24:19 +0100

    NFC: test path

---
+++ run.spec.js
@@ -0,0 +1,12 @@
+import { dsSetUrl } from './';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('set server-url function', () => {
+
+  it('should run', () => {
+    const result = "Hello world!"
+    expect(result).to.equal('Hello world!');
+  });
+
+});
