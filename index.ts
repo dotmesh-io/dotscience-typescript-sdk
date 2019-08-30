442d142611e68d2865cad0091c5c96f534d80b62
Charlotte Godley <charlotte@dotmesh.com>
2019-08-30 11:20:35 +0100

    FIX: build errors

--- index.ts
+++ index.ts
@@ -11,37 +11,31 @@ export function dsSetUrl (hostname: string) : string {
   return ""
 }
 
-// TODO where the hell is the writeable type
-async function writeToWritable(writable: any, apiKey: string) {
-  await streamWrite(writable, `${apiKey}\n`);
-}
-
-export function dsLogin (apiKey: string, username: string) : string {
+export async function dsLogin (apiKey: string, username: string) {
   const child = spawn('ds', ['login', username], {stdio: ['pipe', process.stdout, process.stderr]});
 
-  writeToWriteable(child.stdin, apiKey)
-  child.stdout.on('data', (data) => {
-    console.log(data.toString());
-  });
-  return ""
+  if(child.stdin != null) {
+    await streamWrite(child.stdin, `${apiKey}\n`)
+    if(child.stdout != null) {
+        child.stdout.on('data', (data: object) => {
+        console.log(data.toString());
+      });
+    }
+  }
+  
 }
 /**
 * @Method: Runs a Dotscience command task.
 * @Param {string}
 * @Return {string}
 */
-export function dsRun (apiKey: string, username: string, hostname: string, project: string, command: string, image: string) : string {
+export async function dsRun (apiKey: string, username: string, hostname: string, project: string, command: string, image: string) {
   // set the url first - if blank, don't set it
   if(hostname != "") {
     dsSetUrl(hostname)
   }
   // login
-  dsLogin(apiKey, username)
+  await dsLogin(apiKey, username)
   // run ds run!
   const child = spawn('ds', ['run', '-p', project, '-I', image, '--', command]);
-
-  child.stdout.on('data', (data) => {
-    console.log(data.toString());
-  });
-  return ""
 }
